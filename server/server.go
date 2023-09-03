7c08cd21ce7e2c17a56011e8828732f8c237e3fb
iybe <iesleybezerra2@gmail.com>
2023-09-03 11:36:16 -0300

    adicionando mux router

--- server/server.go
+++ server/server.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"net/http"
 	"pubsub/handlers"
+
+	"github.com/gorilla/mux"
 )
 
 func Run() {
 	handlers := handlers.NewHandlers()
 
-	http.HandleFunc("/message/send/", handlers.SendMessage)
-	http.HandleFunc("/message/receive/", handlers.ReceiveMessages)
+	r := mux.NewRouter()
+	r.HandleFunc("/message/send/{resourceId}", handlers.SendMessage)
+	r.HandleFunc("/message/receive/{resourceId}", handlers.ReceiveMessages)
+	http.Handle("/", r)
 
 	fmt.Println("Server is listening on :8080")
 	http.ListenAndServe(":8080", nil)
